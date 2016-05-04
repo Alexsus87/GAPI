622df4b101c5b37ef82b8d3c56a983af2d116e76
Oleksii Nikonov (Globallogic Inc) <v-olniko@microsoft.com>
2016-05-04 11:52:46 +0300

    Added Name, database update, datetime culture on FinalizeBooking page.

---
+++ BritishCab/Migrations/201605040824158_ChangedTitles.cs
@@ -0,0 +1,16 @@
+namespace BritishCab.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedTitles : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
